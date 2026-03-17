8f631fa00c2ef600d441e2a5e7c14766a80c0ee6
elecbug <deveb1479@gmail.com>
2026-03-17 10:55:37 +0900

    feat: class in 03.17.

---
+++ My Integer/my integer.c
@@ -0,0 +1,24 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+
+#include "my_integer.h"
+
+my_integer add(my_integer i, my_integer j) {
+	return i + j;
+}
+
+my_integer minus(my_integer i, my_integer j) {
+	return i - j;
+}
+
+my_integer mult(my_integer i, my_integer j) {
+	return i * j;
+}
+
+my_integer divid(my_integer i, my_integer j) {
+	if (i % j != 0) {
+		printf("Divide error\n");
+		return 0;
+	}
+	else return (my_integer)(i / j);
+}
---
+++ My Integer/my_integer.h
@@ -0,0 +1,7 @@
+typedef unsigned int my_integer;
+
+// 2개의 정수 i,j를 받아서 합의 되돌려 줌
+extern my_integer add(my_integer i, my_integer j);
+extern my_integer minus(my_integer i, my_integer j);
+extern my_integer mult(my_integer i, my_integer j);
+extern my_integer divid(my_integer i, my_integer j);
